4554d5b52f1ad56b4d691000920a926fc9d6d331
ragnell <ragnellrks@gmail.com>
2023-11-02 11:47:54 +0100

    Second commit 02-11-2023

--- app/src/main/java/es/riberadeltajo/buscaminas/Casilla.java
+++
@@ -1,4 +0,0 @@
-package es.riberadeltajo.buscaminas;
-
-public class Casilla {
-}
--- app/src/main/java/es/riberadeltajo/buscaminas/MainActivity.java
+++ app/src/main/java/es/riberadeltajo/buscaminas/MainActivity.java
@@ -5,29 +5,58 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.gridlayout.widget.GridLayout;
+
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    public int [][] tablero;
+    public int[][] tablero;
+    public Button[] botones;
+    public int minasTotales = 10;
     public int dificultad = 0;
+
+    //Layouts
+    GridLayout g;
+    ConstraintLayout clayout;
+    LinearLayout.LayoutParams layoutParams;
+
+
+    int altura;
+    int ancho;
+
+    int filaColumna;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        filaColumna = 8;
+
+
+
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-
+        g=new GridLayout(this);
+        clayout= findViewById(R.id.layoutPrincipal);
+        altura = clayout.getHeight();
+        ancho = clayout.getWidth();
+        pintarTablero();
     }
 
     @Override
@@ -51,6 +80,38 @@ public class MainActivity extends AppCompatActivity {
 
     //Método que crea el constructor de dialogo del menu de configuracion.
     private void crearMenuConfig() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.configmenu);
+        String[] items = {getString(R.string.principiante),getString(R.string.amateur),getString(R.string.experto)};
+        int seleccion = dificultad;
+        builder.setSingleChoiceItems(items, seleccion, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                    switch (which) {
+                        case 0:
+
+                            dificultad = 0;
+                            minasTotales = 10;
+                            break;
+                        case 1:
+                            dificultad = 1;
+                            minasTotales = 30;
+                            break;
+                        case 2:
+                            dificultad = 2;
+                            minasTotales = 60;
+                            break;
+                    }
+
+            }
+        });
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        mostrarDialogo(builder);
     }
 
     //Método para crear el constructor de dialogo de las Instrucciones.
@@ -76,5 +137,35 @@ public class MainActivity extends AppCompatActivity {
 
     public void pintarTablero(){
         //Esto iniciará/reseteará el juego
+
+
+
+
+
+        //Parametros del layout
+        GridLayout.LayoutParams param=new GridLayout.LayoutParams();
+        param.setMargins(0,0,0,0);
+        param.height= ViewGroup.LayoutParams.MATCH_PARENT;
+        param.width= ViewGroup.LayoutParams.MATCH_PARENT;
+        g.setRowCount(filaColumna);
+        g.setColumnCount(filaColumna);
+        g.setLayoutParams(param);
+
+        Log.d("ASASDASDASSD", "Clayout: "+clayout.getHeight()+" : "+clayout.getWidth());
+        Log.d("ASASDASDASSD", "GRID: "+g.getHeight()+" : "+g.getWidth());
+        //Parametros de botones
+        layoutParams = new LinearLayout.LayoutParams(ancho/filaColumna,altura/filaColumna);
+        layoutParams.setMargins(0,0,0,0);
+
+        for(int i = 0; i<8; i++){
+            for(int j = 0; j < 8; j++){
+                Button b = new Button(this);
+                b.setLayoutParams(layoutParams);
+                b.setBackgroundColor(Color.RED);
+
+                g.addView(b);
+            }
+        }
+
     }
 }
