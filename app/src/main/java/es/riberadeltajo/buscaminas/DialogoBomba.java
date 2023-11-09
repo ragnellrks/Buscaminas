20a971e486a6dd2738d9a5024e1dff4a52e92a65
ragnell <ragnellrks@gmail.com>
2023-11-09 20:08:59 +0100

    VERSION FINALIZADA!

--- app/src/main/java/es/riberadeltajo/buscaminas/DialogoBomba.java
+++ app/src/main/java/es/riberadeltajo/buscaminas/DialogoBomba.java
@@ -4,9 +4,13 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -29,24 +33,63 @@ public class DialogoBomba extends DialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.selecBomba);
-        TipoBombas[] objetoBombas = {new TipoBombas(R.drawable.bomba, "Bomba Clásica"), new TipoBombas(R.drawable.dinamita, "Dinamita"), new TipoBombas(R.drawable.molotov,"Molotov")};
-        ArrayAdapter<TipoBombas> miAdaptador = new ArrayAdapter<TipoBombas> ( getContext (), android.R.layout.simple_spinner_item, objetoBombas  );
-        /*miAdaptador.setDropDownViewResource(R.layout.dropdownmodel);
-        View miVistaSpinner = getLayoutInflater().inflate(R.layout.spinnerlayout, null);
-        Spinner miSpinner = (Spinner) miVistaSpinner.findViewById(R.id.spinnerBomba);*/
-        //miSpinner.setAdapter(miAdaptador);
+        TipoBombas[] objetoBombas = {new TipoBombas(R.drawable.bomba, "Bomba Clásica"),
+                new TipoBombas(R.drawable.dinamita, "Dinamita"),
+                new TipoBombas(R.drawable.molotov,"Molotov")};
+
+        //miAdaptador.setDropDownViewResource(R.layout.dropdownmodel);
+        Spinner miSpinner = new Spinner(getActivity());
+        miSpinner.setAdapter(new MiAdaptadorBombas(getActivity(), R.layout.spinneritems , objetoBombas));;
         builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                //TipoBombas seleccionado = (TipoBombas) miSpinner.getSelectedItem();
-               // drawableBomba = seleccionado.imagen;
-                //selectBomb.onSeleccionDrawable(drawableBomba);
+                TipoBombas seleccionado = (TipoBombas) miSpinner.getSelectedItem();
+               drawableBomba = seleccionado.imagen;
+                selectBomb.onSeleccionDrawable(drawableBomba);
             }
         });
-        //builder.setView(miVistaSpinner);
+        builder.setView(miSpinner);
         return builder.create();
     }
 
+    public class MiAdaptadorBombas extends ArrayAdapter<TipoBombas>{
+        TipoBombas[] misObjetos;
+        public MiAdaptadorBombas(@NonNull Context context, int resource, @NonNull TipoBombas[] objects) {
+            super(context, resource, objects);
+            misObjetos = objects;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            return crearFila(position,convertView,parent);
+        }
+
+
+        //EN CASO DE REALIZARSE CON UN SPINNER
+        @Override
+        public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            return crearFila(position, convertView, parent);
+        }
+
+        private View crearFila(int position, View convertView, ViewGroup parent) {
+            //Obtenemos el layout inflater
+            LayoutInflater miInflador = getLayoutInflater();
+            //Creamos una vista con el inflado del layout inflater, donde elegimos el layout que hemos creado.
+            View vistaFila = miInflador.inflate(R.layout.spinneritems, parent, false);
+
+            //Dentro de esta vista, encontramos las referencias de los objetos de cada fila inflada.
+            TextView nombre = vistaFila.findViewById(R.id.nomBomba);
+            ImageView imagen = vistaFila.findViewById(R.id.icoBomba);
+
+            //Rellenamos los datos con el objeto position del array de objeto.
+            nombre.setText(misObjetos[position].nombre);
+            imagen.setImageResource(misObjetos[position].imagen);
+
+            //Devolvemos la vista con los datos.
+            return vistaFila;
+        }
+    }
     public interface OnSeleccionBomba{
         public void onSeleccionDrawable(int drawable);
     }
--- app/src/main/java/es/riberadeltajo/buscaminas/MainActivity.java
+++ app/src/main/java/es/riberadeltajo/buscaminas/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
@@ -23,30 +24,36 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements DialogoDificultad.OnSeleccionDificultad, DialogoBomba.OnSeleccionBomba{
 
-
+    //Matriz que contiene los números.
     public int[][] tablero;
+    //Total de minas por defecto inicializado a 10
     public int minasTotales = 10;
+    //Controlador de dificultad
     public int dificulty = 0;
+    //Controlador de Aciertos para ganar
     public int aciertos;
-
+    //Filas y Columnas, que en este caso son el mismo valor.
+    int filaColumna;
+    //ID de la imagen Bomba
+    int drawableBomba;
+    //Controlador de derrota
     public boolean derrota;
+    //Item del menu que selecciona la imagen de la bomba
+    MenuItem imagenBomba;
 
-    //Layouts
+    //Layout y parametros.
     GridLayout g;
     ConstraintLayout clayout;
     LinearLayout.LayoutParams layoutParams;
-
     GridLayout.LayoutParams param;
-    int drawableBomba;
-
     int altura;
     int ancho;
-
-    int filaColumna;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,8 +70,9 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
     protected void onStart() {
         super.onStart();
 
-
+        //Obtenemos el Constraint Layout
         clayout= findViewById(R.id.layoutPrincipal);
+        //Necesitamos un runnable para conseguir sus parametros cuando esté dibujado.
         clayout.post(new Runnable() {
             @Override
             public void run() {
@@ -75,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
                 param.setMargins(0,0,0,0);
                 param.height= ViewGroup.LayoutParams.MATCH_PARENT;
                 param.width= ViewGroup.LayoutParams.MATCH_PARENT;
-
+                //Cuando obtenemos sus parametros, pintamos el tablero.
                 pintarTablero();
             }
         });
@@ -90,6 +98,7 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Condiciones para saber que botón del menú se ha pulsado
         if(item.getItemId()==R.id.menuInstrucciones){
             crearDialogoInstrucciones();
         }else if(item.getItemId()==R.id.menuConfig){
@@ -98,11 +107,13 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
             pintarTablero();
             Toast.makeText(this, "El juego se ha reiniciado!", Toast.LENGTH_LONG).show();
         }else if(item.getItemId()==R.id.selectBomba){
+            imagenBomba = item;
             crearMenuBomba();
         }
         return super.onOptionsItemSelected(item);
     }
 
+    //Crea el menú para seleccionar bomba.
     private void crearMenuBomba() {
         DialogoBomba dialogoSeleccion = new DialogoBomba();
         dialogoSeleccion.show(getSupportFragmentManager(), "Selección bomba");
@@ -140,6 +151,7 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
     }
 
     public void pintarTablero(){
+        //Cada vez que se pinte el tablero, significa que hemos iniciado una partida nueva, por lo que devolvemos los valores del juego a 0
         derrota = false;
         aciertos = 0;
        //Es importante borrar la instancia de G en el ConstraintLayout cada vez que se llame a esta función para repintar el tablero.
@@ -151,7 +163,7 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
         g.setRowCount(filaColumna);
         g.setColumnCount(filaColumna);
         g.setLayoutParams(param);
-        g.setBackgroundColor(Color.BLACK);
+        g.setBackgroundColor(Color.parseColor("#f7adf5"));
         //Parametros de botones
         layoutParams = new LinearLayout.LayoutParams(ancho/filaColumna,altura/filaColumna);
         layoutParams.setMargins(0,0,0,0);
@@ -161,6 +173,7 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Si no has perdido o ganado puedes hacer click
                 if(!derrota || aciertos == g.getChildCount() - minasTotales) {
                     boolean found = false;
                     int columna = 0;
@@ -180,9 +193,11 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
                             }
                         }
                     }
+                    //Comprobamos el valor que hay en el tablero en esa posición.
                     switch(tablero[fila][columna]){
                         case -1:
-                            v.setBackgroundColor(Color.RED);
+                            v.setBackgroundResource(R.drawable.explosion);
+                            revelarCampo();
                             Toast.makeText(getApplicationContext(), "HAS PERDIDO!", Toast.LENGTH_LONG).show();
                             derrota = true;
                             break;
@@ -202,6 +217,50 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
             }
         };
 
+        View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                //Si no has perdido o ganado puedes hacer click
+                if (!derrota || aciertos == g.getChildCount() - minasTotales) {
+                    boolean found = false;
+                    int columna = 0;
+                    int fila = 0;
+                    //Si encuentra un hijo en la posicion I que concuerde con el source de la View, se ejecuta.
+                    for (int i = 0; !found || i != g.getChildCount(); i++) {
+                        if (g.getChildAt(i) == v) {
+                            found = true;
+                            Log.d("Ribera del tajo", "Encontrada la vista en posición" + i);
+                            //Sacamos la posicion a la que equivale el botón en la matriz del campo.
+                            if (i == 0) {
+                                fila = 0;
+                                columna = 0;
+                            } else {
+                                fila = i / filaColumna;
+                                columna = i % filaColumna;
+                            }
+                        }
+                    }
+                    //Comprobamos el valor que hay en el tablero en esa posición.
+                    switch (tablero[fila][columna]) {
+                        case -1:
+                            v.setBackgroundResource(drawableBomba);
+                            break;
+                        default:
+                            Button b = (Button) v;
+                            b.setText(Integer.toString(tablero[fila][columna]));
+                            revelarCampo();
+                            Toast.makeText(getApplicationContext(), "HAS PERDIDO!", Toast.LENGTH_LONG).show();
+                            derrota = true;
+                            b.setClickable(false);
+                            break;
+                    }
+
+                }
+                return true;
+            }
+        };
+
+
         //creamos el Tablero
         crearCampoMinas();
 
@@ -215,12 +274,15 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
                     ImageButton ib = new ImageButton(this);
                     ib.setLayoutParams(layoutParams);
                     ib.setOnClickListener(listener);
-                    ib.setBackgroundColor(Color.BLUE);
+                    ib.setOnLongClickListener(longClickListener);
+                    ib.setBackgroundResource(R.drawable.boton);
                     g.addView(ib);
                 }else{
                     Button b = new Button(this);
                     b.setLayoutParams(layoutParams);
                     b.setOnClickListener(listener);
+                    b.setOnLongClickListener(longClickListener);
+                    b.setBackgroundResource(R.drawable.boton);
                     g.addView(b);
                 }
 
@@ -229,6 +291,25 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
 
     }
 
+    private void revelarCampo() {
+        for(int i = 0; i<g.getChildCount(); i++){
+            View v = g.getChildAt(i);
+            if(v instanceof ImageButton){
+                ((ImageButton) v).setBackgroundResource(R.drawable.explosion);
+            }else if(v instanceof Button){
+                if(i != 0){
+                    int fila = i/ filaColumna;
+                    int columna = i%filaColumna;
+                    ((Button) v).setText(Integer.toString(tablero[fila][columna]));
+                }else{
+                    ((Button) v).setText(Integer.toString(tablero[0][0]));
+                }
+
+            }
+        }
+    }
+
+    //Comprueba si has ganado.
     private void checkForWin() {
         if(aciertos == g.getChildCount() - minasTotales){
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -244,6 +325,7 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
         }
     }
 
+    //Revela las casillas contiguas.
     private void revealSurroundings(int i, int j) {
         for (int k = i - 1; k <= i + 1; k++) {
             for (int l = j - 1; l <= j + 1; l++) {
@@ -262,6 +344,7 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
     }
 
 
+    //crear campo de minas
     private void crearCampoMinas() {
         tablero = new int[filaColumna][filaColumna];
 
@@ -289,6 +372,7 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
 
     }
 
+    //Comprueba si hay minas cerca para añadir al contador y aumentar el numero para la casilla
     private int checkForMina(int i, int j) {
         int contador = 0;
         for (int k = i - 1; k <= i + 1; k++) {
@@ -301,6 +385,7 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
         return contador;
     }
 
+    //Interfaces
     @Override
     public void onSeleccion(int dificultad, int minas, int filaColumn ) {
         dificulty = dificultad;
@@ -311,7 +396,6 @@ public class MainActivity extends AppCompatActivity implements DialogoDificultad
     @Override
     public void onSeleccionDrawable(int drawable) {
         drawableBomba = drawable;
-        MenuItem menuBomba = findViewById(R.id.selectBomba);
-        menuBomba.setIcon(drawableBomba);
+        imagenBomba.setIcon(drawable);
     }
 }
